00700e1c6b82f0ce31903b91429954d8ccedfc89
AJ-Kulundu <AJ-Kulundu>
2024-02-15 15:09:54 +0300

    rspec user model test script

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@email.com",
+      password: "Anything")
+  }
+  it "should save with all attributes" do
+    expect(subject).to be_valid
+  end
+  it "should not save without a first name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+  it "should not save without a last name" do
+    last_name = subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should not save without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  it "should not save without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
